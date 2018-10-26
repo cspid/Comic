7dea99a00e8965f0c55467ebc2f86de90b0ef469
Carl <carlospidla@gmail.com>
2018-10-25 21:47:48 -0400

    Borderzzz - set with button in inspector - enable check bordersize to resize in Update

---
+++ Assets/Editor/CustomInspectorBorder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BorderScript))]
+
+public class CustomInspectorBorder : Editor 
+{
+
+	public override void OnInspectorGUI() 
+	{
+
+		DrawDefaultInspector();
+		BorderScript borderScript = (BorderScript)target;
+
+		if (GUILayout.Button("Check for Cameras"))
+        {
+            borderScript.CheckForCams();
+        }
+	}
+
+}
