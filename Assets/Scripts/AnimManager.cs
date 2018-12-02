b154b7d8a57bf2353d49ff397d59e59912bc7ac7
Carl Spidla <carlospidla@gmail.com>
2018-12-01 21:24:30 -0500

    Sat night

--- Assets/Scripts/AnimManager.cs
+++ Assets/Scripts/AnimManager.cs
@@ -2,48 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimManager : MonoBehaviour {
+public class AnimManager : MonoBehaviour
+{
 
-	public Animator middleCamScene1;
-	public Animator bottomCamScene1;
-	public BorderScript borderScript;
+    public Animator middleCamScene1;
+    public Animator bottomCamScene1;
+    public Animator title;
+    public BorderScript borderScript;
+    public triggerDialogue dialogue;
 
-	public bool houseIntro;
-	public bool saabAssembly;
-	public bool unfurlBottom;
+    public bool houseIntro;
+    public bool saabAssembly;
+    bool unfurlBottom;
+    public bool openTitle;
+    public bool closeTitle;
+    public bool blink;
+    public bool text1;
 
-	public Transform bottomCam;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Transform bottomCam;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (houseIntro == true)
-		{
-			middleCamScene1.SetTrigger("House Intro");
-			houseIntro = false;
-		}
 
-		if (saabAssembly == true)
-		{
-			//bottomCam.GetComponent<Camera>().rect = new Rect(0.03f, 0.02f, 0.94f, 0.3066f);
+    // Use this for initialization
+    void Start()
+    {
 
-			bottomCamScene1.SetTrigger("Saab Assembly");
-			saabAssembly = false;
-		}
-        
-		if (unfurlBottom == true)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (houseIntro == true)
         {
-			//bottomCam.GetComponent<Camera>().rect = new Rect(0.03f, 0.3066f, 0.94f, 0f);
-			print("set");
-			bottomCam.gameObject.SetActive(true);
+            middleCamScene1.SetTrigger("House Intro");
+            houseIntro = false;
+        }
+
+        if (saabAssembly == true)
+        {
+            //bottomCam.GetComponent<Camera>().rect = new Rect(0.03f, 0.02f, 0.94f, 0.3066f);
+
+            bottomCamScene1.SetTrigger("Saab Assembly");
+            saabAssembly = false;
+        }
+
+        if (unfurlBottom == true)
+        {
+            //bottomCam.GetComponent<Camera>().rect = new Rect(0.03f, 0.3066f, 0.94f, 0f);
+            print("set");
+            bottomCam.gameObject.SetActive(true);
             bottomCamScene1.SetTrigger("Unfurl");
             unfurlBottom = false;
         }
-	}
+
+        if (openTitle == true)
+        {
+            print("In");
+            title.SetTrigger("In");
+        }
+
+        if (closeTitle == true)
+        {
+            print("out");
+            title.SetTrigger("Out");
+        }
+
+        if (text1 == true)
+        {
+            dialogue.RunDialogue("Intro");
+            print("dialogue1");
+            text1 = false;
+        }
+
+        if (blink == true)
+        {
+
+        }
+
+
+
+    }
 }
