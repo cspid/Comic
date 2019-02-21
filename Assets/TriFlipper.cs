622a22a18d1a32da685fac7beb3809d80ebebe92
cspidla <carlospidla@gmail.com>
2019-02-21 12:15:40 -0500

    tris

---
+++ Assets/TriFlipper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriFlipper : MonoBehaviour
+{
+    float rotationAmount = 90.0f;
+    public bool turn;
+    MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+
+    void Update()
+    {
+        if(turn == true) { 
+        transform.Rotate (rotationAmount * Time.deltaTime, 0, 0, Space.Self);
+            print(transform.localRotation.x);
+            if (transform.localRotation.x >= 100)
+            {
+                renderer.enabled = false;
+                turn = false;
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+    } 
+}
