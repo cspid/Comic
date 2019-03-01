7bc48569a58edd20f6c5d169df0687eacdc5bab4
SJH607-11W\Graphic <carlospidla@gmail.com>
2019-02-28 22:59:31 -0500

    lots of progress

--- Assets/Scripts/DemoSequence.cs
+++ Assets/Scripts/DemoSequence.cs
@@ -1,58 +1,141 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
 public class DemoSequence : MonoBehaviour
 {
+    [Header("General")]
+
+    public int counter = 0;
+    public SpeechBubbleSwitch bubble;
+    public GameObject scene1;
+    public GameObject scene2;
+
+    [Header("Scene 1")]
     public lerpRectTranform openL;
     public lerpRectTranform openBR;
     public lerpRectTranform openT;
     public lerpRectTranform openBL;
-    public SpeechBubbleSwitch bubble;
+    public lerpRectTranform closeTitle;
+    public lerpRectTranform closeT;
+    public lerpRectTranform closeBR;
+    public lerpRectTranform closeBL;
+    public lerpRectTranform closeL;
+    public Text textCounter;
+    public GameObject frames;
+    bool newSceneTrigger;
+    public ballStop stop;
+    float timer = 3;
+
+    [Header("Scene 2")]
+    public lerpRectTranform openL_S2;
+    public lerpRectTranform openM_S2;
+    public lerpRectTranform openR_S2;
 
 
-    public int counter = 0;
+    [Header("Yarn")]
+    public DialogueRunner dialogueRunner;
+
+
     bool callLerp;
     int counterLastFrame;
-    public ballStop stop;
+    public ColorLerp colorLerp;
 
     // Update is called once per frame
     void Update()
     {
+        if(newSceneTrigger == true)
+        {
+            NewScene();
+        }
+
         if (Input.GetButtonDown("Fire1"))
         {
             counter++;
         }
+
         if (counter > counterLastFrame)
         {
+            textCounter.text = "" + counter;
             if (counter == 1)
             {
-             //   openL.isLerping = true;
+             // Open text  
                 openT.isLerping = true;
                 bubble.isSpeech = true;
-
+                dialogueRunner.StartDialogue("Thesis_Start");
 }
-
-
             if (counter == 2)
             {
-                openBR.isLerping = true;
+                //Open BR
+               openBR.isLerping = true;
+               closeTitle.isLerping = true;
             }
-
 
             if (counter == 3)
             {
-                //Kill the ball and
-                stop.active = true;
-
+                //dialogue
             }
 
-
             if (counter == 4)
+            {
+            }
+            if (counter == 5)
+
+            {               
+                //Kill the ball and
+                stop.active = true;
+            }
+
+            if (counter == 6)
             {
                 // open new frame
                 openBL.isLerping = true;
             }
+
+            if (counter == 7)
+            {
+                // open new frame
+                dialogueRunner.StartDialogue("Thesis_Scene_1B");
+            }
+
+            if (counter == 7)
+            {
+             
+                bubble.isSpeech = true;
+            }
+
+            if (counter == 12)
+            {
+                //Shutdown scene 1
+                closeT.isLerping = true;
+                closeBL.isLerping = true;
+                closeBR.isLerping = true;
+                closeL.isLerping = true;
+                newSceneTrigger = true;
+                
+            }
         }
         counterLastFrame = counter; 
+    }
+    void NewScene()
+    {
+        timer = timer - Time.deltaTime;
+        if(timer < 0)
+        {
+            foreach (Transform child in frames.transform)
+            {
+                DestroyImmediate(child.gameObject);
+                colorLerp.enabled = true;
+            }
+            scene2.SetActive(true);
+        
+            openL_S2.isLerping = true;
+            openM_S2.isLerping = true;
+            openR_S2.isLerping = true;
+            newSceneTrigger = false;
+
+            scene1.SetActive(false);
+        }
     }
 }
